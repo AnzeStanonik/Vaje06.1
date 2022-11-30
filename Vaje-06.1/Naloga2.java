74d4b57ec717d84d6177232999211d6dab4f0d00
Smotko <114558526+Smotko@users.noreply.github.com>
2022-11-30 08:45:37 +0100

    Add files via upload

--- Vaje-06.1/Naloga2.java
+++ Vaje-06.1/Naloga2.java
@@ -1,18 +1,16 @@
 import java.util.Random;
-class Naloga1 {
+class Naloga2 {
     public static void main(String[] args) {
         Random r = new Random();
         int[] a = new int[5];
         int sestevek = 0;
         int j = 0;
+        int b;
         for(int i = 0; i < a.length; i++) {
             a[i] = r.nextInt();
             j = i + 1;   
             System.out.println("Stevilo " + j + " = " + a[i]);
-            sestevek += a[i];
         }
-        int povprecje = sestevek / a.length;
-        System.out.println("Skuna vrednost = " + sestevek);
-        System.out.println("Povprecje = " + povprecje);
+        System.out.println(Math.min(a[1], a[2]));
     }
 }
